6c317e75540c64e610360450e3a2ca3db8f99534
Fae Hamilton-Raine <faehamiltonraine@gmail.com>
2025-02-18 13:49:32 -0700

    Added TasksComponent 🕶️

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,25 +1,24 @@
-import { Component, signal, computed } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
 
-  // get imagePath() { 
-  //   return 'assets/users/' + this.selectedUser().avatar;
-  // }
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    this.select.emit(this.id);
   }
 }
