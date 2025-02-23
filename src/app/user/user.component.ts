e930b7f3ca12dabe1a32ac41225d81542a17ada7
Fae Hamilton-Raine <faehamiltonraine@gmail.com>
2025-02-23 07:34:10 -0700

    Added stuff

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,6 +2,17 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
 
+// type User = {
+//     id: string;
+//     name: string;
+//     avatar: string;
+//   }
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -9,16 +20,18 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: User;
+
   @Output() select = new EventEmitter();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
